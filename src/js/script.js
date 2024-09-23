0de67bab43fac0312be42ccc9b9148b330170e5b
Ilya <ilya02989@gmail.com>
2024-09-23 11:50:37 +0300

    animations

--- src/js/script.js
+++ src/js/script.js
@@ -118,6 +118,18 @@ $(document).ready(function () {
         });
         return false;
     });
+
+
+    //smooth scroll and pageup
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    new WOW().init();
 });
 
 
